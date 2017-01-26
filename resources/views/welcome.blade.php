5510479c0ed1ab3281ed0d3321968e1050001882
AlexanderCK3 <alexanderck3@gmail.com>
2017-01-26 13:40:19 -0600

    save view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Custom Deploy LOCAL
+                    Custom Deploy 
                 </div>
 
                 <div class="links">
